71199a3d6b4bd5b0fb58372ae91d417d3bfeac6b
SHEF <Shefqetmush@gmail.com>
2022-11-14 06:12:41 -0500

    display weather info

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ async function getWeatherData(location, state, country) {
     `http://api.openweathermap.org/data/2.5/weather?q=${location},${state},${country}&APPID=7a12fe1d21939af0deb6d706dcb169ec`
   );
   const data = await response.json();
+  return displayWeather(data);
+}
+
+function displayWeather(data) {
   console.log(data, data.name, data.sys.country);
 }
 
