0ffc3ea8e34b8b6a4f3a944af72e794e951b3a67
SHEF <Shefqetmush@gmail.com>
2022-11-14 06:07:23 -0500

    initialize project wrote function to hit api

---
+++ index.js
@@ -0,0 +1,9 @@
+async function getWeatherData(location, state, country) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${location},${state},${country}&APPID=7a12fe1d21939af0deb6d706dcb169ec`
+  );
+  const data = await response.json();
+  console.log(data, data.name, data.sys.country);
+}
+
+getWeatherData('Baghdad', '', 'Iraq');
