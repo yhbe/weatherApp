e166b90af091c7931c52a972cc75f56144470216
SHEF <Shefqetmush@gmail.com>
2022-11-15 08:11:34 -0500

    getweatherData function and form listener

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-async function getWeatherData(location, state, country) {
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${location},${state},${country}&APPID=7a12fe1d21939af0deb6d706dcb169ec`
-  );
-  const data = await response.json();
-  return displayWeather(data);
+async function getWeatherData(str) {
+  let location = str[0];
+  let state = str[1];
+  let country = str[2];
+  try {
+    if (country === undefined && state === undefined) {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=7a12fe1d21939af0deb6d706dcb169ec`
+      );
+      const data = await response.json();
+      return displayWeather(data);
+    }
+    if (country === undefined) {
+      country = state;
+      state = '';
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${location},${country}&APPID=7a12fe1d21939af0deb6d706dcb169ec`
+      );
+      const data = await response.json();
+      return displayWeather(data);
+    } else {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${location},${state},${country}&APPID=7a12fe1d21939af0deb6d706dcb169ec`
+      );
+      const data = await response.json();
+      return displayWeather(data);
+    }
+  } catch {
+    console.log('that didnt work...err');
+  }
 }
 
 function displayWeather(data) {
   console.log(data, data.name, data.sys.country);
 }
 
-getWeatherData('Baghdad', '', 'Iraq');
+const form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const input = document.querySelector('form > input');
+  let str = input.value.split(',');
+  getWeatherData(str);
+});
